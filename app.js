e4d9f885691e319d51a25486c6ddf36bb67e54b8
Vishal Jyoti <135093607+VishalJyoti15071@users.noreply.github.com>
2023-05-31 18:28:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
+import {Component} from 'react'
 import './App.css'
 
 // Replace your code here
-const App = () => <div>Hello World</div>
+class App extends Component {
+  state = {activeTheme: 'light', savedVideos: []}
+
+  render() {
+    return (
+      <>
+        <>
+          <div className="bg-container">
+            <h1>Hello</h1>
+          </div>
+        </>
+      </>
+    )
+  }
+}
 
 export default App
-
